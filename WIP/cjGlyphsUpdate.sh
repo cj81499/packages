24ca957cdae9bab05ed3bce185e1b690823b2da8
Cal Jacobson <cj81499@users.noreply.github.com>
2019-10-04 15:39:53 -0400

    Relocate Glyphs

---
+++ WIP/cjGlyphsUpdate.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+cd /Users/Cal/Documents/Jailbreak_Stuff/cj81499.github.io/Glyphs/Library/Themes
+
+echo Removing .DS_Store.
+find . -name ".DS_Store" -depth -exec rm {} \;
+
+echo Removing Old Files.
+rm cjGlyphs.zip
+
+echo Zipping cjGlyphs.
+zip -r -X cjGlyphs.zip cjGlyphs*
+
+echo Done. Exiting.
+exit 0
