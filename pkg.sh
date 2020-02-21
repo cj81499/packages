c1f2c946ef13c79a5e5dd60c921ded98941431a9
Cal Jacobson <cj81499@users.noreply.github.com>
2020-02-21 18:55:06 -0500

    update pkg.sh
    update control files for cj Glyphs and Glyphs

--- pkg.sh
+++ pkg.sh
@@ -1,6 +1,6 @@
 # Check for args
 if [ $# -ne 1 ]; then
-    echo "usage: $0 package_name"
+    echo "usage: $0 package_id"
     exit 1
 fi
 
@@ -15,7 +15,7 @@ rm $1.deb
 rm $1.zip
 
 echo "Creating deb"
-dpkg-deb -b $1/src $1.deb
+dpkg-deb --build $1/src $1.deb
 
 echo "Creating zip"
 cd $1/src
